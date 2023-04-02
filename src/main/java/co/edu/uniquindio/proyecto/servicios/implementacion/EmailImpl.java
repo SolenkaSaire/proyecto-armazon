94c2b3698698008b4a6de64425104259179951cf
cristian <Cristianr.gonzalezi@uqvirtual.edu.co>
2023-04-02 13:17:46 -0500

    mas clases

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/EmailImpl.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+import co.edu.uniquindio.proyecto.dto.EmailDTO;
+import co.edu.uniquindio.proyecto.servicios.interfaces.EmailServicio;
+
+public class EmailImpl implements EmailServicio {
+    @Override
+    public String enviarEmail(EmailDTO emailDTO) {
+        return null;
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/SesionServicioImpl.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+import co.edu.uniquindio.proyecto.dto.SesionDTO;
+import co.edu.uniquindio.proyecto.dto.TokenDTO;
+import co.edu.uniquindio.proyecto.servicios.interfaces.SesionServicio;
+
+public class SesionServicioImpl implements SesionServicio {
+    @Override
+    public TokenDTO login(SesionDTO sesionDTO) {
+        return null;
+    }
+
+    @Override
+    public void logout(int codigoUsuario) {
+
+    }
+}

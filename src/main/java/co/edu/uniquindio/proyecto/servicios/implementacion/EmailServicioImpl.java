d1ac052be90df7254f06c98cf9b8a4a9a2396285
cristian <Cristianr.gonzalezi@uqvirtual.edu.co>
2023-04-06 22:49:30 -0500

    mas

---
+++ src/main/java/co/edu/uniquindio/proyecto/servicios/implementacion/EmailServicioImpl.java
@@ -0,0 +1,26 @@
+import co.edu.uniquindio.proyecto.dto.EmailDTO;
+import co.edu.uniquindio.proyecto.servicios.interfaces.EmailServicio;
+import jakarta.mail.internet.MimeMessage;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+@Service
+@AllArgsConstructor
+public class EmailServicioImpl implements EmailServicio {
+    private final JavaMailSen
+    @Override
+    public void enviarEmail(EmailDTO emailDTO) throws Exception{
+        MimeMessage mensaje = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mensaje);
+        try{
+            helper.setSubject(emailDTO.getAsunto());
+            helper.setText(emailDTO.getCuerpo(), true);
+            helper.setTo(emailDTO.getDestinatario());
+            helper.setFrom("no_reply@dominio.com");
+            javaMailSender.send(mensaje);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/co/edu/uniquindio/proyecto/servicios/implementacion/ProductoServicioImplTest.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.proyecto.servicios.implementacion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoServicioImplTest {
+
+    @Test
+    void crearProducto() {
+    }
+
+    @Test
+    void testCrearProducto() {
+    }
+}
